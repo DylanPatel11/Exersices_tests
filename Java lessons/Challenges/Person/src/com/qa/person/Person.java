e159ececf4ca9a37a576436b7899e81e135496ae
DylanPatel11 <me@dylanpatel.co.uk>
2022-03-02 16:13:53 +0000

    DP: 1st commit

---
+++ Java lessons/Challenges/Person/src/com/qa/person/Person.java
@@ -0,0 +1,14 @@
+package com.qa.person;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	private String jobTitle;
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", jobTitle=" + jobTitle + "]";
+	} 
+	
+
+}
